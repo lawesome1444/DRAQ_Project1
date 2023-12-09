395f91ef7c75a66c817ba6cf640f5156a76e6561
Conor <pacman1444@gmail.com>
2023-12-09 23:08:39 +0000

    Added Legal component as a footer

---
+++ src/components/legal.js
@@ -0,0 +1,12 @@
+//Basic "legal" info, just meant to stay at the bottom as a footer.
+function Legal(){
+    return(
+        <div>
+            <p>
+                This is not an actual store. It is meant to demonstrate routing.<br/>
+                Rights go to Valve and the respected developers for hosting the game images.
+            </p>
+        </div>
+    );
+}
+export default Legal;
