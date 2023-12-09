254522ca180b780e5cb267ab6197afb5cb7f0819
Conor <pacman1444@gmail.com>
2023-12-09 23:39:12 +0000

    Created a store component. Adding Client routing

--- src/App.js
+++ src/App.js
@@ -2,19 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 //Importing Footer. Permanent element with Legal info
 import Legal from './components/legal';
+//Importing Routable Components
+import Store from './components/store';
 //Importing Bootstrap and Router DOM for client side routing
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-
+//Set the background colour theme
+document.body.style = 'background: #58A0EB;';
 
 function App() {
   return (
-    <div className="App">
-      <Legal></Legal>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        {/*Creating the Navbar, adding different page options and pointing where each option goes to*/}
+        <Navbar bg="dark" data-bs-theme="dark">
+            <Container>
+              <Navbar.Brand href="/">GotNoGames</Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link href="/store">Store</Nav.Link>
+              </Nav>
+            </Container>
+        </Navbar>
+        <br/>
+        {/*Telling which content to load depending on what page we are on (e.g DRAQ_Project1/store loads the store page HTML code)*/}
+        <Routes>
+          <Route path='/store' element={<Store></Store>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/store.js
@@ -0,0 +1,13 @@
+//Used for displaying games.
+    //Does not use an API yet. This needs to be addressed in due time. (AKA TO-DO!!!)
+//Basic "legal" info, just meant to stay at the bottom as a footer.
+function Store(){
+    return(
+        <div>
+            <h1>Game 1</h1>
+            <h1>Game 2</h1>
+            <h1>Game 3</h1>
+        </div>
+    );
+}
+export default Store;
