554ac2b0685d77bef23e737da37401dc1d83d3e3
Conor <pacman1444@gmail.com>
2023-12-09 23:12:43 +0000

    Installed and imported Bootstrap and Router DOM

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 //Importing Footer. Permanent element with Legal info
 import Legal from './components/legal';
+//Importing Bootstrap and Router DOM for client side routing
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
