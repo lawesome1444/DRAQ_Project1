eda73d5a2d2552a0a5dfc0167ff02b9dd0eed411
Conor <pacman1444@gmail.com>
2023-12-11 00:54:57 +0000

    Further improvements to cards

--- src/components/gameEntries.js
+++ src/components/gameEntries.js
@@ -4,9 +4,13 @@ import { Link } from 'react-router-dom';
 function GameEntries(props){
     return(
         <div className="App">
-            <Card style={{ width: '42rem' }}>
+            <Card style={{ width: '42rem',
+            background: 'linear-gradient(to right, #F05511, #9C0902)',
+            padding: '10px 10px 10px 10px',
+            borderRadius: '15px 15px 15px 15px',
+            }}>
             <Card.Title>{props.gameDetails.title}</Card.Title>
-            <img src={props.gameDetails.boxArt}></img>
+            <img src={props.gameDetails.boxArt} style={{ borderRadius: '15px 15px 15px 15px'}}></img>
             <p>{props.gameDetails.desc}</p>
             </Card>
             <br/>
