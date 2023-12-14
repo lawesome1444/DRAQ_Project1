5688c22e11dc18f338d6aa1071fad71b7c53a332
Conor <pacman1444@gmail.com>
2023-12-14 20:38:57 +0000

    store.js's page now allows users to search for games

--- src/components/store.js
+++ src/components/store.js
@@ -6,6 +6,9 @@ function Store(){
 
     //Document by document, get an entry off the database, send it to games.js, format it then send it back to be displayed on this page
     const [data, setData] = useState([]);
+    //Used to store filtered data from the above, in this case for storing games that match the search
+    const [dataSearch, setDataSearch] = useState('');
+
 
     //Sync with DB to get up to date entries
     useEffect(
@@ -46,6 +49,19 @@ function Store(){
         )
     }
 
+    /*Filters all by lower-case so it is case-insensitive. Used to make the searching experience better.
+    I was inspired by the top answer in this Stack Overflow question regarding case-insensitive filters.
+    https://stackoverflow.com/questions/48979908/react-filter-function-case-insensitive
+    */
+    const gameSearch = data.filter(game => 
+        game.title.toLowerCase().includes(dataSearch.toLowerCase())
+    );
+
+    //When the user types in a new value in the search bar, change the filtered game list value (dataSearch)
+    const doGameSearch = (event) => {
+        setDataSearch(event.target.value);
+    };
+
 
     return(
         //Display the cards that follow in the center of the store page
@@ -56,8 +72,14 @@ function Store(){
                 borderRadius: '5px 5px 5px 5px',
                 margin: 'auto', width: '60%'
             }}>Store</h2>
+            <input 
+                type="text" 
+                placeholder="Search for a game..." 
+                value={dataSearch}
+                onChange={doGameSearch}
+            />
             <br/>
-            <Games gameDetails={data} Reload={ReloadStore}></Games>
+            <Games gameDetails={gameSearch} Reload={ReloadStore}></Games>
         </div>
     );
 }
